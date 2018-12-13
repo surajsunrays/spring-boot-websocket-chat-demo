7596b4aa5b8129997b2bdfc6915e8e71050e823b
suraj <suraj.bodade@zemosolabs.com>
2018-12-13 12:01:08 +0530

    Script file edited with conditional options

--- dockertest.sh
+++ dockertest.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-#this script is used for checking and deploying the docker container
-if [ $(docker inspect -f '{{.State.Running}}' spring-boot-websocket) = "true" ]; then 
-echo "The Container is already running " 
-echo "Now Stopping the container to Create the latest" 
-sudo docker container stop spring-boot-websocket
-echo "Now Removing the container from docker to release resources---"
-sudo docker container rm -f spring-boot-websocket
-echo "----- Now Reinstalling the container ----- "
-sudo docker run -d --name spring-boot-websocket -p 7070:8080 spring-boot-app
-echo "---- Container running and listening at port 7070--------"
-echo "-----------OPERATION COMPLETE----"; 
-else echo "No Such Container Running"
-if [ $(docker inspect -f '{{.State.Exited}}' spring-boot-websocket) = "true" ];then
-echo "------- Found One Exited Container -----------"
-sudo docker container rm -f spring-boot-websocket;
-echo "----- Exited container removed------"; 
-else 
-echo "Nothing Found"; fi
-echo "----------------Creating the new Container-------------"
-sudo docker run -d --name spring-boot-websocket -p 7070:8080 spring-boot-app; fi
+#This script is used to check the various states of docker container
 
-echo "----------------------Operation finish------------------"
+#Checking the already runnung container
+if [ $(docker inspect -f '{{.State.Running}}' spring-boot-websocket)="false" ]
+then
+    echo "-----Container Running-------------";
+    echo "-----Stopping the container---------";
+    docker container stop spring-boot-websocket;
+    echo "Now Cleaning the Existing container resources......";
+    docker container rm -f spring-boot-websocket
+    echo "=--   Now Launching the New Container -----=";
+    #Starting the new Container Here
+    docker run -d --name spring-boot-websocket -p 7070:8080 spring-boot-app;
+    
+elif [ $(docker inspect -f '{{.State.Exited}}' spring-boot-websocket)="false" ]
+then
+    echo "Container found in exited state";
+    echo "Now Cleaning the Existing container resources......";
+    docker container rm -f spring-boot-websocket
+    echo "=--   Now Launching the New Container -----=";
+    #Starting the new Container Here
+    docker run -d --name spring-boot-websocket -p 7070:8080 spring-boot-app;
+else
+    #Starting the new Container Here
+    echo "------------Creating the new Container--------------"
+    docker run -d --name spring-boot-websocket -p 7070:8080 spring-boot-app;
+fi
+echo "--**-----**------ Operation Complete------**-----**--------";
